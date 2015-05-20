1a244aa825865694467b04813440019953422ee5
ADAM-M <adamamatheny@gmail.com>
2015-05-20 19:34:20 -0400

    More source files

---
+++ GI_UnitySource/Assets/AsteroidCollision.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidCollision : MonoBehaviour 
+{
+
+	public GameObject pieceA;
+	public GameObject pieceB;
+	public GameObject pieceC;
+	public GameObject pieceD;
+	public GameObject pieceE;
+	public GameObject pieceF;
+	public GameObject pieceG;
+
+
+	public GameObject mLaserFistEmblem;
+	public GameObject mBigBlastEmblem;
+
+	void OnTriggerEnter(Collider other)
+	{
+		
+		if (other.gameObject.GetComponent<PlayerBulletController> () != null) 
+		{
+			AsteroidDeath();
+			Destroy(other.gameObject);
+		}
+	}
+
+	public void AsteroidDeath()
+	{
+		//Update the Asteroid death count
+		PlayerPrefs.SetInt("AsteroidCount", PlayerPrefs.GetInt("AsteroidCount") + 1);
+
+		GameObject newAsteroidBit = Instantiate(pieceA, transform.position, Quaternion.identity) as GameObject;
+		newAsteroidBit.transform.localScale = transform.parent.localScale;
+		newAsteroidBit.transform.localScale = transform.parent.localScale;
+		newAsteroidBit = Instantiate(pieceB, transform.position, Quaternion.identity) as GameObject;
+		newAsteroidBit.transform.localScale = transform.parent.localScale;
+		newAsteroidBit = Instantiate(pieceC, transform.position, Quaternion.identity) as GameObject;
+		newAsteroidBit.transform.localScale = transform.parent.localScale;
+		newAsteroidBit = Instantiate(pieceD, transform.position, Quaternion.identity) as GameObject;
+		newAsteroidBit.transform.localScale = transform.parent.localScale;
+		newAsteroidBit = Instantiate(pieceE, transform.position, Quaternion.identity) as GameObject;
+		newAsteroidBit.transform.localScale = transform.parent.localScale;
+		newAsteroidBit = Instantiate(pieceF, transform.position, Quaternion.identity) as GameObject;
+		newAsteroidBit.transform.localScale = transform.parent.localScale;
+		newAsteroidBit = Instantiate(pieceD, transform.position, Quaternion.identity) as GameObject;
+		newAsteroidBit.transform.localScale = transform.parent.localScale;
+
+		newAsteroidBit = Instantiate(pieceA, transform.position, Quaternion.identity) as GameObject;
+		newAsteroidBit.transform.localScale = transform.parent.localScale;
+		newAsteroidBit = Instantiate(pieceB, transform.position, Quaternion.identity) as GameObject;
+		newAsteroidBit.transform.localScale = transform.parent.localScale;
+		newAsteroidBit = Instantiate(pieceC, transform.position, Quaternion.identity) as GameObject;
+		newAsteroidBit.transform.localScale = transform.parent.localScale;
+		newAsteroidBit = Instantiate(pieceD, transform.position, Quaternion.identity) as GameObject;
+		newAsteroidBit.transform.localScale = transform.parent.localScale;
+		newAsteroidBit = Instantiate(pieceE, transform.position, Quaternion.identity) as GameObject;
+		newAsteroidBit.transform.localScale = transform.parent.localScale;
+		newAsteroidBit = Instantiate(pieceF, transform.position, Quaternion.identity) as GameObject;
+		newAsteroidBit.transform.localScale = transform.parent.localScale;
+		newAsteroidBit = Instantiate(pieceD, transform.position, Quaternion.identity) as GameObject;
+		newAsteroidBit.transform.localScale = transform.parent.localScale;
+
+		//float spawnChance = Random.Range(1,1000);
+		//if(spawnChance == 777)
+		if(PlayerPrefs.GetInt("AsteroidCount") >= PlayerPrefs.GetInt("AsteroidRequiredCount"))
+		{
+			if(PlayerPrefs.GetInt("SpawnLaserFist") == 0)
+			{
+				Instantiate(mLaserFistEmblem, new Vector3(transform.position.x, transform.position.y, -2f), Quaternion.identity);
+				PlayerPrefs.SetInt("SpawnLaserFist",1);
+			}
+			else if(PlayerPrefs.GetInt("SpawnLaserFist") == 1)
+			{
+				Instantiate(mBigBlastEmblem, new Vector3(transform.position.x, transform.position.y, -2f), Quaternion.identity);
+				PlayerPrefs.SetInt("SpawnLaserFist",0);
+			}
+
+			PlayerPrefs.SetInt("AsteroidRequiredCount", PlayerPrefs.GetInt("AsteroidRequiredCount") + Random.Range(175,250));
+
+		}
+
+		Destroy(transform.parent.gameObject);
+	}
+}
---
+++ GI_UnitySource/Assets/AsteroidS/AsteroidScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidScript : MonoBehaviour {
+
+	public int duration = 30;
+
+	float speed;
+	float scaleSize;
+	float zedPos;
+
+	public Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () 
+	{
+		//This line was to create an  Asteroid counter in the player prefs
+//		PlayerPrefs.SetInt("AsteroidCount", 0);
+//		PlayerPrefs.SetInt("AsteroidRequiredCount", 100);
+//		PlayerPrefs.SetInt("SpawnLaserFist",0);
+
+		speed = Random.Range (-1, -10);
+
+		zedPos = Random.Range (-3, 1.68f);
+		transform.position = new Vector3 (transform.position.x, transform.position.y, zedPos);
+
+		scaleSize = Random.Range (8, 20);
+		transform.localScale = new Vector3 (scaleSize, scaleSize, 1f);
+	
+		rb = GetComponent<Rigidbody2D>();
+
+		rb.velocity = new Vector2 (rb.velocity.x, speed);
+		rb.angularVelocity = Random.Range (-200, 150);
+		Destroy (gameObject, duration);
+	}
+}
