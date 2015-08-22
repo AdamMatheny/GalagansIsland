d9064c5f8b6ab0f569399896b306ad31e55b0980
AdamMatheny <adamamatheny@gmail.com>
2015-08-22 18:25:04 -0400

    -Updated how the controllable boss rotates its lasers -Updated how the AI hero ship dodges -Put borders into the LudumDare level

--- Assets/Ludum Dare #33/Scripts/Boss/BossBulletRotater.cs
+++ Assets/Ludum Dare #33/Scripts/Boss/BossBulletRotater.cs
@@ -1,18 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BossBulletRotater : MonoBehaviour {
+public class BossBulletRotater : MonoBehaviour 
+{
+	public Vector3 mTargetRotation = Vector3.zero;
+	public Vector3 newDir;
+	public float mRotateSpeed = 10f;
+	void Start()
+	{
 
-	public Rigidbody2D rgb2d;
-
-	void Start(){
-
-		rgb2d = GetComponent<Rigidbody2D> ();
 	}
 
-	void Update(){
+	void Update()
+	{
+		mTargetRotation = new Vector3(Input.GetAxis ("RightAnalogHorizontal"), Input.GetAxis ("RightAnalogVertical"), 0);
+		Vector3.Normalize (mTargetRotation);
+		mTargetRotation = new Vector3(0f, 0f, Vector3.Angle(mTargetRotation, Vector3.down));
+		if(Input.GetAxis ("RightAnalogHorizontal") < 0f)
+		{
+			mTargetRotation *= -1f;
+		}
+		if(Input.GetAxis ("RightAnalogHorizontal") !=0f || Input.GetAxis ("RightAnalogVertical") != 0f)
+		{
+	
 
-		transform.rotation = new Quaternion(Input.GetAxis ("RightAnalogHorizontal"), Input.GetAxis ("RightAnalogVertical"), 0, 0);
+			transform.rotation =Quaternion.Lerp (transform.rotation, Quaternion.Euler (mTargetRotation), 0.001f*mRotateSpeed);
+		}
+		//transform.rotation = new Quaternion(Input.GetAxis ("RightAnalogHorizontal"), Input.GetAxis ("RightAnalogVertical"), 0, 0);
 	}
 	
 }
--- Assets/Ludum Dare #33/Scripts/Player/HeroShipAI.cs
+++ Assets/Ludum Dare #33/Scripts/Player/HeroShipAI.cs
@@ -5,6 +5,7 @@ public class HeroShipAI : MonoBehaviour
 {
 	public Transform mTarget;
 	public GameObject mHeroBullet;
+	public GameObject mDodgeObject;
 
 	public float mSpeed = 16f;
 	public Vector3 mMoveDir = Vector3.zero;
@@ -32,6 +33,7 @@ public class HeroShipAI : MonoBehaviour
 		}
 		else
 		{
+			mMoveDir = Vector3.Normalize (transform.position-mDodgeObject.transform.position);
 			mDodgeTimer -= Time.deltaTime;
 		}
 
@@ -44,6 +46,7 @@ public class HeroShipAI : MonoBehaviour
 
 		//Try to get under whatever is shooting at us ~Adam
 		mMoveDir *= mSpeed * 0.01f;
+		mMoveDir = new Vector3(mMoveDir.x, mMoveDir.y, 0f);
 
 		//Move the ship ~Adam
 		transform.Translate(mMoveDir);
@@ -52,18 +55,22 @@ public class HeroShipAI : MonoBehaviour
 	void OnTriggerEnter(Collider other)
 	{
 
-		if(other.gameObject != this.gameObject)
+		if(other.gameObject != this.gameObject && other.tag != "Player Bullet")
 		{
+			Debug.Log ("enter "+other.gameObject.name);
 			mDodgeTimer = 1f;
-			mMoveDir = Vector3.Normalize (transform.position-other.transform.position);
+			mDodgeObject = other.gameObject;
+			mMoveDir = Vector3.Normalize (transform.position-mDodgeObject.transform.position);
 		}
 	}
 	void OnTriggerStay(Collider other)
 	{
-		if(other.gameObject != this.gameObject)
+		if(other.gameObject != this.gameObject && other.tag != "Player Bullet")
 		{
+			Debug.Log ("Stay "+other.gameObject.name);
 			mDodgeTimer = 1f;
-			mMoveDir = Vector3.Normalize (transform.position-other.transform.position);
+			mDodgeObject = other.gameObject;
+			mMoveDir = Vector3.Normalize (transform.position-mDodgeObject.transform.position);
 		}
 	}
 
