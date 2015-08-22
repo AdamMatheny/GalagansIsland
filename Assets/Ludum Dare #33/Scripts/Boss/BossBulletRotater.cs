5115260a3058db9cf127c50dc148f52305c467ae
Nidaoke <jnidaokefantagefacebook27@gmail.com>
2015-08-22 16:37:27 -0400

    Glitch

---
+++ Assets/Ludum Dare #33/Scripts/Boss/BossBulletRotater.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossBulletRotater : MonoBehaviour {
+
+	public Rigidbody2D rgb2d;
+
+	void Start(){
+
+		rgb2d = GetComponent<Rigidbody2D> ();
+	}
+
+	void Update(){
+
+		transform.rotation = new Quaternion(Input.GetAxis ("RightAnalogHorizontal"), Input.GetAxis ("RightAnalogVertical"), 0, 0);
+	}
+	
+}
