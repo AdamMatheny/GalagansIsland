ff9ee0b653dacde2722501ecf1a878c654ccd542
AdamMatheny <adamamatheny@gmail.com>
2015-08-22 13:53:49 -0400

    -Made a basic AI and prefab for the hero ship for Ludum Dare build --The ship will try to move to being below a targte (has to be set in the editor at the moment) 	And will try to move away from anything that gets close to it

---
+++ Assets/Ludum Dare #33/Scripts/Player/HeroShipAI.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeroShipAI : MonoBehaviour 
+{
+	public Transform mTarget;
+	public GameObject mHeroBullet;
+	public float mSpeed = 16f;
+	public Vector3 mMoveDir = Vector3.zero;
+	
+	public float mDodgeTimer = 0f;
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(mDodgeTimer <= 0f)
+		{
+			mMoveDir = Vector3.Normalize ((mTarget.position+(Vector3.down*20f))-transform.position);
+
+		}
+		else
+		{
+			mDodgeTimer -= Time.deltaTime;
+		}
+
+		//Try to get under whatever is shooting at us ~Adam
+		mMoveDir *= mSpeed * 0.01f;
+
+		//Move the ship ~Adam
+		transform.Translate(mMoveDir);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+
+		if(other.gameObject != this.gameObject)
+		{
+			Debug.Log ("Enter "+ other.gameObject.name);
+			mDodgeTimer = 1f;
+			mMoveDir = Vector3.Normalize (transform.position-other.transform.position);
+		}
+	}
+	void OnTriggerStay(Collider other)
+	{
+		if(other.gameObject != this.gameObject)
+		{
+			Debug.Log ("Stay "+ other.gameObject.name);
+			mDodgeTimer = 1f;
+			mMoveDir = Vector3.Normalize (transform.position-other.transform.position);
+		}
+	}
+}
