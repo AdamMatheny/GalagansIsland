3ac287364b4f4b82ccd37b27f2591ecf7b5162f3
Nidaoke <jnidaokefantagefacebook27@gmail.com>
2015-08-23 17:52:31 -0400

    Merge of AI and Particles

---
+++ Assets/LDBulletScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LDBulletScript : EnemyBulletController {
+	
+	public void OnCollisionEnter(Collision other){
+		
+		if (other.gameObject.tag == "Player") {
+			
+			Destroy(gameObject);
+		}
+	}
+}
--- Assets/Ludum Dare #33/Scripts/Boss/BossEye.cs
+++ Assets/Ludum Dare #33/Scripts/Boss/BossEye.cs
@@ -3,11 +3,45 @@ using System.Collections;
 
 public class BossEye : MonoBehaviour {
 
+	public GameObject BuildUp;
+	
+	public GameObject mTarget;
+	
+	public GameObject bullet;
+	
+	public float timer;
+	float timerTemp;
+	
+	public void Start(){
+		
+		mTarget = GameObject.FindGameObjectWithTag ("Player");
+		
+		timerTemp = timer;
+	}
+	
 	public void Update(){
 
+		if (timerTemp < 3) {
+
+			BuildUp.SetActive (true);
+		} else {
+
+			BuildUp.SetActive(false);
+		}
+		
+		if (timerTemp > 0) {
+			
+			timerTemp -= Time.deltaTime;
+		} else {
+			
+			timerTemp = timer;
+			Instantiate(bullet, transform.position + new Vector3(0, 6), Quaternion.identity);
+			Debug.Log("SHOOT!");
+		}
+		
 		float horizontal = Input.GetAxis ("RightAnalogHorizontal");
 		float vertical = Input.GetAxis ("RightAnalogVertical");
-
+		
 		transform.localPosition = new Vector2 (horizontal / 15, (vertical / 15) + .04f);
 	}
 }
