dcda2f0f47ea33094af424f05b3cf47a0b2b5097
AdamMatheny <adamamatheny@gmail.com>
2015-08-24 16:41:09 -0400

    -Updated skull boss to use new sprites and animations -Fixed a bug where the beams could only hit the red ship

--- Assets/Ludum Dare #33/Scripts/Boss/LDBulletScript.cs
+++ Assets/Ludum Dare #33/Scripts/Boss/LDBulletScript.cs
@@ -8,43 +8,46 @@ public class LDBulletScript : EnemyBulletController
 	public void OnCollisionEnter(Collision other)
 	{
 		
-		if (other.gameObject.tag == "Player") 
-		{
-			if(other.gameObject.GetComponent<HeroShipAI>().mInvincibleTimer <= 0f)
-			{
-				other.gameObject.GetComponent<HeroShipAI>().HitHeroShip(mHitDamage);
-			}
-			Destroy(gameObject);
-		}
+//		if (other.gameObject.tag == "Player") 
+//		{
+//			if(other.gameObject.GetComponent<HeroShipAI>().mInvincibleTimer <= 0f)
+//			{
+//				other.gameObject.GetComponent<HeroShipAI>().HitHeroShip(mHitDamage);
+//			}
+//		}
 
-		else if (other.gameObject.name == "ShipCore") 
+		//else
+		if (other.gameObject.name == "ShipCore") 
 		{
 			if(other.transform.parent.gameObject.GetComponent<HeroShipAI>().mInvincibleTimer <= 0f)
 			{
 				other.transform.parent.gameObject.GetComponent<HeroShipAI>().HitHeroShip(mHitDamage);
 			}
+			Destroy(gameObject);
 		}
 	}
 
 	public void OnTriggerEnter(Collider other)
 	{
 		
-		if (other.gameObject.tag == "Player") 
-		{
-			if(other.gameObject.GetComponent<HeroShipAI>().mInvincibleTimer <= 0f)
-			{
-				other.gameObject.GetComponent<HeroShipAI>().HitHeroShip(mHitDamage);
-			}
-			Destroy(gameObject);
-		}
+//		if (other.gameObject.tag == "Player") 
+//		{
+//			if(other.gameObject.GetComponent<HeroShipAI>().mInvincibleTimer <= 0f)
+//			{
+//				other.gameObject.GetComponent<HeroShipAI>().HitHeroShip(mHitDamage);
+//			}
+//		}
 
 
-		else if (other.gameObject.name == "ShipCore") 
+		//else
+		if (other.gameObject.name == "ShipCore") 
 		{
 			if(other.transform.parent.gameObject.GetComponent<HeroShipAI>().mInvincibleTimer <= 0f)
 			{
 				other.transform.parent.gameObject.GetComponent<HeroShipAI>().HitHeroShip(mHitDamage);
 			}
+			transform.GetChild(0).SetParent (null);
+			Destroy(gameObject);
 		}
 	}
 }
