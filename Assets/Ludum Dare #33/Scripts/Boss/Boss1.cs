e7953a05fa06a9030cca88142db9c7ca5e845a86
Nidaoke <jnidaokefantagefacebook27@gmail.com>
2015-08-23 23:25:23 -0400

    Second Boss Work

--- Assets/Ludum Dare #33/Scripts/Boss/Boss1.cs
+++ Assets/Ludum Dare #33/Scripts/Boss/Boss1.cs
@@ -3,5 +3,40 @@ using System.Collections;
 
 public class Boss1 : BossGenericScript {
 
-	//Put boss specialy code here
+	public bool leftHornAlive = true;
+	public bool rightHornAlive = true;
+	
+	public SpriteRenderer spriter;
+	
+	public Sprite rightHorn;
+	public Sprite leftHorn;
+	public Sprite noHorns;
+
+	public override void Start ()
+	{
+		spriter = GetComponent<SpriteRenderer> ();
+		base.Start ();
+	}
+
+	public override void Update ()
+	{
+		if (!leftHornAlive && rightHornAlive) {
+			
+			spriter.sprite = rightHorn;
+		}else{
+			
+			if(leftHornAlive && !rightHornAlive){
+				
+				spriter.sprite = leftHorn;
+			}else{
+				
+				if(!leftHornAlive && !rightHornAlive){
+					
+					spriter.sprite = noHorns;
+				}
+			}
+		}
+
+		base.Update ();
+	}
 }
--- Assets/Ludum Dare #33/Scripts/Boss/BossGenericScript.cs
+++ Assets/Ludum Dare #33/Scripts/Boss/BossGenericScript.cs
@@ -5,43 +5,21 @@ public class BossGenericScript : MonoBehaviour {
 
 	//Put generic boss code here
 
-	public bool leftHornAlive = true;
-	public bool rightHornAlive = true;
 
-	public SpriteRenderer spriter;
-
-	public Sprite rightHorn;
-	public Sprite leftHorn;
-	public Sprite noHorns;
 
 	//public int health;
 
 	public Rigidbody2D rgb2d;
 
-	public void Start(){
+	public virtual void Start(){
 	
-		spriter = GetComponent<SpriteRenderer> ();
+
 		rgb2d = GetComponent<Rigidbody2D> ();
 	}
 
 	public virtual void Update(){
 
-		if (!leftHornAlive && rightHornAlive) {
 
-			spriter.sprite = rightHorn;
-		}else{
-
-			if(leftHornAlive && !rightHornAlive){
-
-				spriter.sprite = leftHorn;
-			}else{
-
-				if(!leftHornAlive && !rightHornAlive){
-
-					spriter.sprite = noHorns;
-				}
-			}
-		}
 
 		float horizontal = Input.GetAxis ("Horizontal");
 		float vertical = Input.GetAxis ("Vertical");
