3558a7029267bd14c173ee8a7dc7d967be8e5613
Nidaoke <jnidaokefantagefacebook27@gmail.com>
2015-08-25 21:30:55 -0400

    Hornet Boss / Music Fix

---
+++ Assets/Ludum Dare #33/Scripts/Boss/HornSkullBoss/LDStinger.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LDStinger : MonoBehaviour {
+
+	public int mHitDamage = 1;
+
+	public void OnTriggerEnter(Collider other)
+	{
+		
+		//		if (other.gameObject.tag == "Player") 
+		//		{
+		//			if(other.gameObject.GetComponent<HeroShipAI>().mInvincibleTimer <= 0f)
+		//			{
+		//				other.gameObject.GetComponent<HeroShipAI>().HitHeroShip(mHitDamage);
+		//			}
+		//		}
+		
+		
+		//else
+		if (other.gameObject.name == "ShipCore") 
+		{
+			Debug.Log (gameObject.name + " hit ship core");
+			if(other.transform.parent.gameObject.GetComponent<HeroShipAI>().mInvincibleTimer <= 0f)
+			{
+				other.transform.parent.gameObject.GetComponent<HeroShipAI>().HitHeroShip(mHitDamage);
+			}
+			transform.GetChild(0).SetParent (null);
+			Destroy(gameObject);
+		}
+	}
+}
