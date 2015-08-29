9f97676fcdc27f195cc4aa0f9ce2bda1878f1182
Nidaoke <jnidaokefantagefacebook27@gmail.com>
2015-08-29 00:01:14 -0400

    Visual Hindrance

--- Assets/Ludum Dare #33/Scripts/Boss/HornSkullBoss/LDStinger.cs
+++ Assets/Ludum Dare #33/Scripts/Boss/HornSkullBoss/LDStinger.cs
@@ -3,7 +3,14 @@ using System.Collections;
 
 public class LDStinger : MonoBehaviour {
 
+	public GameObject cameraShader;
+
 	public int mHitDamage = 1;
+
+	public void Start(){
+
+		cameraShader = GameObject.FindGameObjectWithTag ("MainCamera");
+	}
 
 	public void OnTriggerEnter(Collider other)
 	{
@@ -20,6 +27,9 @@ public class LDStinger : MonoBehaviour {
 		//else
 		if (other.gameObject.name == "ShipCore") 
 		{
+			cameraShader.GetComponent<CameraShader> ().shader1.enabled = true;
+			cameraShader.GetComponent<CameraShader> ().shader2.enabled = true;
+
 			Debug.Log (gameObject.name + " hit ship core");
 			if(other.transform.parent.gameObject.GetComponent<HeroShipAI>().mInvincibleTimer <= 0f)
 			{
@@ -27,6 +37,15 @@ public class LDStinger : MonoBehaviour {
 			}
 			transform.GetChild(0).SetParent (null);
 			Destroy(gameObject);
+		}
+	}
+
+	public void OnTriggerExit(Collider other){
+
+		if (other.gameObject.name == "ShipCore") {
+
+			cameraShader.GetComponent<CameraShader> ().shader1.enabled = false;
+			cameraShader.GetComponent<CameraShader> ().shader2.enabled = false;
 		}
 	}
 }
