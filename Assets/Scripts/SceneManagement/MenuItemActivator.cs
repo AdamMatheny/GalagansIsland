c57e9233f9d813d5df5ff97a0c3644c5cd002b30
Nidaoke <jnidaokefantagefacebook27@gmail.com>
2015-09-07 20:41:28 -0400

    Revert "Credits"
    This reverts commit abe390e83a5bc5dfd93a2e5f0e1873f4c9b340ae.

--- Assets/Scripts/SceneManagement/MenuItemActivator.cs
+++ Assets/Scripts/SceneManagement/MenuItemActivator.cs
@@ -5,13 +5,8 @@ public class MenuItemActivator : MonoBehaviour
 {
 	public GameObject mMenuUIController;
 
-	public Vector3 scale;
-
-	public Transform goTo;
-
 	public float mActivationTimer = 0f;
 
-	public bool mainMenu;
 
 	bool mUiActivated = false;
 
@@ -24,29 +19,22 @@ public class MenuItemActivator : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
-		if (mainMenu) {
+		mActivationTimer += Time.deltaTime;
 
-			mActivationTimer += Time.deltaTime;
-		}
-
-		transform.position = Vector3.Lerp(transform.position, goTo.position, 0.01f);
-
+			transform.position = Vector3.Lerp(transform.position, new Vector3(0,40f,-2f), 0.01f);
 		if(!Application.isMobilePlatform)
 		{
-			transform.localScale = Vector3.Lerp(transform.localScale, scale, 0.01f);
+			transform.localScale = Vector3.Lerp(transform.localScale, new Vector3(9f,9f,9f), 0.01f);
 		}
 		else
 		{
 			transform.localScale = Vector3.Lerp(transform.localScale, new Vector3(6.9f,6.9f,6.9f), 0.01f);
 		}
-
-		if (mainMenu) {
-
-			if(mActivationTimer > 8.25f && !mUiActivated)
-			{
-				mMenuUIController.SetActive(true);
-				mUiActivated = true;
-			}
+		if(mActivationTimer > 8.25f && !mUiActivated)
+		{
+			mMenuUIController.SetActive(true);
+			mUiActivated = true;
 		}
+
 	}
 }
