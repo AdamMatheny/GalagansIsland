5fd5cbca01b6fc52f5d16868f3cb3459f298123c
AdamMatheny <adamamatheny@gmail.com>
2015-09-16 15:48:20 -0400

    -Made it so for boss 5 always looks at player during last stage, but eye beam constantly spins on its own (spinning, not tracking) -Made the Duo Boss bullet hitbox match its graphic

--- Assets/Scripts/BossSpecial/GenericBossScripts/Generic/BossRotator.cs
+++ Assets/Scripts/BossSpecial/GenericBossScripts/Generic/BossRotator.cs
@@ -15,10 +15,17 @@ public class BossRotator : MonoBehaviour
 	public GameObject mReferenceRotator;
 	public Vector3 mRefOffset = Vector3.zero;
 
+	public bool mAutoRotate = false;
+	public bool mDisconnect = false;
+	Transform mDisParent;
 	// Use this for initialization
 	void Start () 
 	{
-		
+		if(mDisconnect)
+		{
+			mDisParent = transform.parent.transform;
+			transform.parent = null;
+		}
 	}//END of Start()
 	
 	// Update is called once per frame
@@ -29,24 +36,41 @@ public class BossRotator : MonoBehaviour
 		{
 			transform.rotation = Quaternion.Euler(mReferenceRotator.transform.rotation.eulerAngles +  mRefOffset);
 		}
-		//Spin on its own towards the player ~Adam
+
 		else
 		{
-			if(mBossCentral.mTargetedPlayer != null)
+			//Spin on its own towards the player ~Adam
+			if(!mAutoRotate)
 			{
-				mTargetPlayerPosition = mBossCentral.mTargetedPlayer.transform.position;
-				mTargetRotation = new Vector3(mTargetPlayerPosition.x-transform.position.x,mTargetPlayerPosition.y-transform.position.y,0f);
-				//mTargetRotation += Vector3.up*3f;
-			}
+				if(mBossCentral.mTargetedPlayer != null)
+				{
+					mTargetPlayerPosition = mBossCentral.mTargetedPlayer.transform.position;
+					mTargetRotation = new Vector3(mTargetPlayerPosition.x-transform.position.x,mTargetPlayerPosition.y-transform.position.y,0f);
+					//mTargetRotation += Vector3.up*3f;
+				}
 
-			Vector3.Normalize (mTargetRotation);
-			mTargetRotation = new Vector3(0f, 0f, Vector3.Angle(mTargetRotation, Vector3.down));
-			if(mTargetPlayerPosition.x < transform.position.x)
+				Vector3.Normalize (mTargetRotation);
+				mTargetRotation = new Vector3(0f, 0f, Vector3.Angle(mTargetRotation, Vector3.down));
+				if(mTargetPlayerPosition.x < transform.position.x)
+				{
+					mTargetRotation *= -1f;
+				}
+
+				transform.rotation =Quaternion.Lerp (transform.rotation, Quaternion.Euler (mTargetRotation), 0.001f*mRotateSpeed * Time.timeScale);
+			}
+			//Just spin without regards to the player
+			else
 			{
-				mTargetRotation *= -1f;
-			}
+				if(mDisconnect && mDisParent != null)
+				{
+					transform.position = mDisParent.position;
+				}
+				mTargetRotation = transform.rotation.eulerAngles + new Vector3(0f,0f,mRotateSpeed);
 
-			transform.rotation =Quaternion.Lerp (transform.rotation, Quaternion.Euler (mTargetRotation), 0.001f*mRotateSpeed * Time.timeScale);
+				transform.rotation =Quaternion.Lerp (transform.rotation, Quaternion.Euler (mTargetRotation), 0.5f * Time.timeScale);
+
+
+			}
 		}
 	}//END of Update()
 
