329da0dd99fb637eae0b0e442502edffa5e9c85e
AdamMatheny <adamamatheny@gmail.com>
2015-10-27 13:28:38 -0400

    -Made a prefab for an item to be picked up by the player to launch Tower of Elements in-game -Removed the ToE application files from this repository since we're going to make a separate repository for the IndieGame files.

---
+++ Assets/Scripts/IndieGamesLauncher/IndieGameCollider.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Scripts.IndieGamesLauncher;
+
+public class IndieGameCollider : MonoBehaviour 
+{
+	ScoreManager mScoreMan;
+	public string mGameTitle;
+	public float mDriftSpeed = 5.47f;
+
+	// Use this for initialization
+	void Start () 
+	{
+		transform.position = new Vector3(Random.Range (-10,10), Random.Range (-20,20), -2);
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(mScoreMan == null)
+		{
+			mScoreMan = FindObjectOfType<ScoreManager>();
+		}
+		else if(mScoreMan.mPlayer2Avatar != null || mScoreMan.mP2Score > 0)
+		{
+			Destroy(this.gameObject);
+		}
+		transform.Translate (Vector3.down* mDriftSpeed*Time.deltaTime);
+
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.GetComponent<PlayerOneShipController>() != null)
+		{
+			other.GetComponent<PauseManager>().Pause ();
+			IndieGamesManager.instance.StartGame(mGameTitle);
+			Destroy (this.gameObject);
+		}
+	}
+}
