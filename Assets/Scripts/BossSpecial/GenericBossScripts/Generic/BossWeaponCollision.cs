c3d251c569653985829056c5ed5d73613b0edf0f
AdamMatheny <adamamatheny@gmail.com>
2015-11-18 20:47:59 -0500

    -Made it so the boss beam weapons have a 1.5 second delay upon activation before they can hurt the player in order to let the particle effects come out all the way. -Altered the hitboxes on the twin boss bile beam and the boss 5 electro beam to match with the particle effects better.

--- Assets/Scripts/BossSpecial/GenericBossScripts/Generic/BossWeaponCollision.cs
+++ Assets/Scripts/BossSpecial/GenericBossScripts/Generic/BossWeaponCollision.cs
@@ -7,22 +7,42 @@ public class BossWeaponCollision : MonoBehaviour
 {
 	ScoreManager mScoreMan;
 	[SerializeField] private int mDamage = 3;
+
+	//For putting a delay on how soon the beam can hurt in order to let the particle effect fully extend ~Adam
+		//We'll do a more thorough fix later ~Adam
+	public float mSafeTime = 0f; 
+
 	// Use this for initialization
 	void Start () 
 	{
 		mScoreMan = FindObjectOfType<ScoreManager>();
+		Debug.Log ("Start: Weapon safe to touch");
+		mSafeTime = 0f;
 	}//END of Start()
-	
+
+
+	void Awake()
+	{
+		Debug.Log ("Awake: Weapon safe to touch");
+		mSafeTime = 0f;
+	}
+
+	void OnEnable() 
+	{
+		Debug.Log ("Enabled: Weapon safe to touch");
+		mSafeTime = 0f;
+	}
+
 	// Update is called once per frame
 	void Update () 
 	{
-		
+		mSafeTime += Time.deltaTime;
 	}//END of Update()
 
 
 	void OnTriggerEnter(Collider other)
 	{
-		if(other.GetComponent<PlayerShipController>()!= null && mScoreMan.mPlayerSafeTime <= 0f)
+		if(other.GetComponent<PlayerShipController>()!= null && mScoreMan.mPlayerSafeTime <= 0f  && mSafeTime >= 1.5f)
 		{
 			//Do extra damage ~Adam
 			if(mDamage > 1)
